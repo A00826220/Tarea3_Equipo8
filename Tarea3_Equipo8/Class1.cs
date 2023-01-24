18d93d8d4e5c22a8d99343ce096d7ada65102c10
A00826220 <A00826220@tec.mx>
2023-01-24 08:44:18 -0600

    Add project files.

---
+++ Tarea3_Equipo8/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea3_Equipo8
+{
+    public class GeneradorAlaeatorios
+    {
+        public List<float> aleatorios { get; set; }
+        public int a { get; set; }
+        public int c { get; set; }
+        public int m { get; set; }
+        public float semilla { get; set; }
+    }
+    
+    public void GeneradorAleatorios (int a, int c, int m)
+    {
+        Random aleatorio = new Random(Environment.TickCount);
+        for (int i = 0; i < a; i++)
+        {
+            double value = aleatorio.NextDouble();
+            double value2 = aleatorio.Next()
+        }
+    }
+
+
+    
+}
